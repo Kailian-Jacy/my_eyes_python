2b995cfb79e56493e2bf7e71ebef6b813207d13f
kailian jacy <xza1809884911@163.com>
2023-05-29 23:54:16 +0800

    123123

--- manage.py
+++ manage.py
@@ -2,20 +2,13 @@
 import os
 import sys
 
+import web
+from handler import Handle
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wxcloudrun.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+urls = (
+    '/wx', 'Handle',
+)
 
 if __name__ == '__main__':
-    main()
+    app = web.application(urls, globals())
+    app.run()
