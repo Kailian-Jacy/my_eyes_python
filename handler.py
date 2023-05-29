46d2dfec4732012dd9233bb1a5d19e5a482f4ee4
kailian jacy <xza1809884911@163.com>
2023-05-30 00:22:03 +0800

    123123

--- handler.py
+++ handler.py
@@ -6,20 +6,38 @@ import reply
 import receive
 import web
 class Handle(object):
-    def POST(self):
-        try:
-            webData = web.data()
-            print("Handle Post webdata is ", webData)
-            #后台打日志
-            recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
-                toUser = recMsg.FromUserName
-                fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
-            else:
-                print("暂且不处理")
-                return "success"
-        except Exception:
-            print("exception.")
+	def POST(self):
+		try:
+			webData = web.data()
+			print("Handle Post webdata is ", webData)
+			#后台打日志
+			recMsg = receive.parse_xml(webData)
+			if isinstance(recMsg, receive.Msg):
+				# Echo Server.
+				if recMsg.MsgType == 'text':
+					toUser = recMsg.FromUserName
+					fromUser = recMsg.ToUserName
+					content = recMsg.Content
+					replyMsg = reply.TextMsg(toUser, fromUser, content)
+					return replyMsg.send()
+				# Voice translator.
+				elif recMsg.MsgType == 'voice':
+					toUser = recMsg.FromUserName
+					fromUser = recMsg.ToUserName
+					content = "received voice."
+					replyMsg = reply.TextMsg(toUser, fromUser, content)
+					return replyMsg.send()
+				# Image describer.
+				elif recMsg.MsgType == 'image':
+					toUser = recMsg.FromUserName
+					fromUser = recMsg.ToUserName
+					content = "received image."
+					replyMsg = reply.TextMsg(toUser, fromUser, content)
+					# mediaId = recMsg.MediaId
+					# replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+					return replyMsg.send()
+			else:
+				print("暂且不处理")
+				return "success"
+		except Exception:
+			print("exception.")
